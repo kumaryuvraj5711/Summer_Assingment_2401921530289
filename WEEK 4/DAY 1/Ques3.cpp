75bc2086b4906c667e4723948a2885b94b8708a8
kumaryuvraj5711 <kumaryuvraj5711@gmail.com>
2026-06-30 00:44:31 +0530

    Create Ques3.cpp

---
+++ WEEK 4/DAY 1/Ques3.cpp
@@ -0,0 +1,14 @@
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Both trees are empty
+        if not p and not q:
+            return True
+        
+        # One tree is empty, or values don't match
+        if not p or not q or p.val != q.val:
+            return False
+        
+        # Recursively check left and right subtrees
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
